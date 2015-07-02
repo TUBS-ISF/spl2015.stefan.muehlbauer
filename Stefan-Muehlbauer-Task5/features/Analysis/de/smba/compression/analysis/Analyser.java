b37066b293e1cd75b7fa993ca6e51da7f4dab017
smba <s.muehlbauer@student.ucc.ie>
2015-07-02 20:37:02 +0200

    removed unused imports

--- Stefan-Muehlbauer-Task5/features/Analysis/de/smba/compression/analysis/Analyser.java
+++ Stefan-Muehlbauer-Task5/features/Analysis/de/smba/compression/analysis/Analyser.java
@@ -1,10 +1,7 @@
 package de.smba.compression.analysis;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 
 public class Analyser {
 
