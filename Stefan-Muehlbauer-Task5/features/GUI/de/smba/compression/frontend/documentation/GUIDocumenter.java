c51162a8f089790912ccf3757ed4f8a962ff02c5
smba <s.muehlbauer@student.ucc.ie>
2015-07-07 13:55:04 +0200

    unresolved class name fix

--- Stefan-Muehlbauer-Task5/features/GUI/de/smba/compression/frontend/documentation/GUIDocumenter.java
+++ Stefan-Muehlbauer-Task5/features/GUI/de/smba/compression/frontend/documentation/GUIDocumenter.java
@@ -3,7 +3,7 @@ package de.smba.compression.frontend.documentation;
 import de.smba.compression.frontend.documentation.IDocumenter;
 import de.smba.compression.frontend.documentation.IGUIDocumenter;
 
-public class EmptyDocumenter implements IDocumenter, IGUIDocumenter {
+public class GUIDocumenter implements IDocumenter, IGUIDocumenter {
 
 	public EmptyDocumenter() {
 		
